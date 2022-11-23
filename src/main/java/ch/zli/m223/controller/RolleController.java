cae5d4afffc1187da903c670e7e38b436b792a46
David <david.kresic@swisscom.com>
2022-11-23 09:43:35 +0100

    added all endpoints for rolle

--- src/main/java/ch/zli/m223/controller/RolleController.java
+++ src/main/java/ch/zli/m223/controller/RolleController.java
@@ -1,5 +1,62 @@
 package ch.zli.m223.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.Rolle;
+import ch.zli.m223.service.RolleService;
+
+@Path("/rolle")
+@Tag(name = "Rolle", description = "Handhabung von Rollen")
 public class RolleController {
     
+    @Inject
+    RolleService service;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)   
+    public List<Rolle> getRolle() {
+        return service.getAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Rolle create(Rolle rolle) {
+        return service.makeRolle(rolle);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Rolle getOneRolle(@PathParam("id") Long id) {
+            return service.GetRolleById(id);
+    }
+    
+    //Works but doesnt return any message
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/{id}")
+    public void delete(@PathParam("id") Long id) {
+        service.deleteRolleById(id);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Rolle update(@PathParam("id") Long id, Rolle rolle){
+            return service.updateRolleById(id, rolle);
+    }
 }
--- src/main/java/ch/zli/m223/service/RolleService.java
+++ src/main/java/ch/zli/m223/service/RolleService.java
@@ -1,6 +1,46 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.Rolle;
+
+
+@ApplicationScoped
 public class RolleService {
 
+    @Inject
+    EntityManager entityManager;
+
+    public List<Rolle> getAll() {
+        var anfrage = entityManager.createQuery("FROM Rolle", Rolle.class);
+        return anfrage.getResultList();
+    }
+
+    @Transactional
+    public Rolle makeRolle(Rolle rolle) {
+        entityManager.persist(rolle);
+        return rolle;
+    }
+
+    public Rolle GetRolleById(Long id){
+        var suche = entityManager.find(Rolle.class, id);
+        return suche;
+    }
+
+    @Transactional
+    public void deleteRolleById(Long id) {
+        var suche = entityManager.find(Rolle.class, id);
+        entityManager.remove(suche);
+    }
+
+    @Transactional
+    public Rolle updateRolleById(Long id, Rolle rolle) {
+        return entityManager.merge(rolle);
+    }
 }
     
