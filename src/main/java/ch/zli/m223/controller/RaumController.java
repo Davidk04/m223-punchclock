d7699da225b946c10efba16818f67dac42fa2864
David <david.kresic@swisscom.com>
2022-11-23 09:15:31 +0100

    added all endpoints for raum

--- src/main/java/ch/zli/m223/controller/RaumController.java
+++ src/main/java/ch/zli/m223/controller/RaumController.java
@@ -1,5 +1,61 @@
 package ch.zli.m223.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.Raum;
+import ch.zli.m223.service.RaumService;
+
+@Path("/raum")
+@Tag(name = "Raum", description = "Handhabung von Räumen")
 public class RaumController {
+    @Inject
+    RaumService raumService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)   
+    public List<Raum> getRaum() {
+        return raumService.getAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Raum create(Raum raum) {
+        return raumService.makeRaum(raum);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Raum getOneRaum(@PathParam("id") Long id) {
+            return raumService.GetRaumById(id);
+    }
     
+    //Works but doesnt return any message
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/{id}")
+    public void delete(@PathParam("id") Long id) {
+        raumService.deleteRaumById(id);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Raum update(@PathParam("id") Long id, Raum raum){
+            return raumService.updateRaumById(id, raum);
+    }
 }
--- src/main/java/ch/zli/m223/service/RaumService.java
+++ src/main/java/ch/zli/m223/service/RaumService.java
@@ -1,5 +1,44 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.Raum;
+
+@ApplicationScoped
 public class RaumService {
-    
+
+    @Inject
+    EntityManager entityManager;
+
+    public List<Raum> getAll() {
+        var anfrage = entityManager.createQuery("FROM Raum", Raum.class);
+        return anfrage.getResultList();
+    }
+
+    @Transactional
+    public Raum makeRaum(Raum raum) {
+        entityManager.persist(raum);
+        return raum;
+    }
+
+    public Raum GetRaumById(Long id){
+        var suche = entityManager.find(Raum.class, id);
+        return suche;
+    }
+
+    @Transactional
+    public void deleteRaumById(Long id) {
+        var suche = entityManager.find(Raum.class, id);
+        entityManager.remove(suche);
+    }
+
+    @Transactional
+    public Raum updateRaumById(Long id, Raum raum) {
+        return entityManager.merge(raum);
+    }
 }
