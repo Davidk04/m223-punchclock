9e8850fc1d790ba70c7290e00a866b4df8fb5de3
David <david.kresic@swisscom.com>
2022-11-23 09:49:20 +0100

    added all endpoints for benutzer

--- src/main/java/ch/zli/m223/controller/BenutzerController.java
+++ src/main/java/ch/zli/m223/controller/BenutzerController.java
@@ -1,5 +1,62 @@
 package ch.zli.m223.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.Benutzer;
+import ch.zli.m223.service.BenutzerService;
+
+@Path("/benutzer")
+@Tag(name = "Benutzer", description = "Handhabung von Benutzer")
 public class BenutzerController {
 
+    @Inject
+    BenutzerService service;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)   
+    public List<Benutzer> getBenutzer() {
+        return service.getAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Benutzer create(Benutzer benutzer) {
+        return service.makeBenutzer(benutzer);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Benutzer getOneBenutzer(@PathParam("id") Long id) {
+            return service.GetBenutzerById(id);
+    }
+    
+    //Works but doesnt return any message
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/{id}")
+    public void delete(@PathParam("id") Long id) {
+        service.deleteBenutzerById(id);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Benutzer update(@PathParam("id") Long id, Benutzer benutzer){
+            return service.updateBenutzerById(id, benutzer);
+    }
 }
--- src/main/java/ch/zli/m223/service/BenutzerService.java
+++ src/main/java/ch/zli/m223/service/BenutzerService.java
@@ -1,5 +1,45 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.Benutzer;
+
+
+@ApplicationScoped
 public class BenutzerService {
-    
+
+    @Inject
+    EntityManager entityManager;
+
+    public List<Benutzer> getAll() {
+        var anfrage = entityManager.createQuery("FROM Benutzer", Benutzer.class);
+        return anfrage.getResultList();
+    }
+
+    @Transactional
+    public Benutzer makeBenutzer(Benutzer benutzer) {
+        entityManager.persist(benutzer);
+        return benutzer;
+    }
+
+    public Benutzer GetBenutzerById(Long id){
+        var suche = entityManager.find(Benutzer.class, id);
+        return suche;
+    }
+
+    @Transactional
+    public void deleteBenutzerById(Long id) {
+        var suche = entityManager.find(Benutzer.class, id);
+        entityManager.remove(suche);
+    }
+
+    @Transactional
+    public Benutzer updateBenutzerById(Long id, Benutzer benutzer) {
+        return entityManager.merge(benutzer);
+    }
 }
