4cdc43609bafb3bbbde660df581447e87c3e26f7
David <david.kresic@swisscom.com>
2022-11-23 09:36:49 +0100

    added all endpoints for rang

--- src/main/java/ch/zli/m223/controller/RangController.java
+++ src/main/java/ch/zli/m223/controller/RangController.java
@@ -1,5 +1,62 @@
 package ch.zli.m223.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.Rang;
+import ch.zli.m223.service.RangService;
+
+@Path("/rang")
+@Tag(name = "Rang", description = "Handhabung von Rängen")
 public class RangController {
+    @Inject
+    RangService service;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)   
+    public List<Rang> getRang() {
+        return service.getAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Rang create(Rang rang) {
+        return service.makeRang(rang);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Rang getOneRang(@PathParam("id") Long id) {
+            return service.GetRangById(id);
+    }
     
+    //Works but doesnt return any message
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("/{id}")
+    public void delete(@PathParam("id") Long id) {
+        service.deleteRangById(id);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Rang update(@PathParam("id") Long id, Rang rang){
+            return service.updateRangById(id, rang);
+    }
+
 }
--- src/main/java/ch/zli/m223/service/RangService.java
+++ src/main/java/ch/zli/m223/service/RangService.java
@@ -1,5 +1,45 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.Rang;
+
+@ApplicationScoped
 public class RangService {
+
+    @Inject
+    EntityManager entityManager;
+
+    public List<Rang> getAll() {
+        var anfrage = entityManager.createQuery("FROM Rang", Rang.class);
+        return anfrage.getResultList();
+    }
+
+    @Transactional
+    public Rang makeRang(Rang rang) {
+        entityManager.persist(rang);
+        return rang;
+    }
+
+    public Rang GetRangById(Long id){
+        var suche = entityManager.find(Rang.class, id);
+        return suche;
+    }
+
+    @Transactional
+    public void deleteRangById(Long id) {
+        var suche = entityManager.find(Rang.class, id);
+        entityManager.remove(suche);
+    }
+
+    @Transactional
+    public Rang updateRangById(Long id, Rang rang) {
+        return entityManager.merge(rang);
+    }
     
 }
